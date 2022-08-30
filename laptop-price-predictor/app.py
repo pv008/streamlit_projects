0f543ba720a891c1afc051a44bd9e6adf3d11333
Pankaj verma <105307983+pv008@users.noreply.github.com>
2022-08-30 14:40:11 +0530

    Update app.py

--- laptop-price-predictor/app.py
+++ laptop-price-predictor/app.py
@@ -17,7 +17,7 @@ url2 = "https://www.googleapis.com/drive/v3/files/14-zRh0M2sCNCf4wF_IazLEvrgSyb_
 pipe = pickle.load(urllib.request.urlopen(url1))
 df = pickle.load(urllib.request.urlopen(url2))
 
-st.title("Laptop Predictor")
+st.title("Laptop Price Predictor")
 
 # brand
 company = st.selectbox('Brand',df['Company'].unique())
